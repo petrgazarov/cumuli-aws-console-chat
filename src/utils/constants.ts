414e3d51ad130428bb52442bb800b63010a5a45d
Petr Gazarov <petrgazarov@gmail.com>
2023-12-10 15:03:33 -0800

    Change supported domain namespace; copy changes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,6 @@ export const CHAT_CHANNEL = "llm-channel";
 
 export const COMMAND_CHANNEL = "command-channel";
 
-export const AWS_CONSOLE_HOST = "console.aws.amazon.com";
+export const AWS_CONSOLE_HOST = "aws.amazon.com";
 
 export const OPENAI_API_KEY_STORAGE_KEY = "openai-api-key";
