e8de6533b839a2f04680a4bd37db5ab73fc1e64b
Petr Gazarov <petrgazarov@gmail.com>
2023-12-12 00:04:54 -0800

    Bump version in manifest; add version patch script

---
+++ scripts/update-manifest-version.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+const packageJson = require("../package.json");
+
+const manifestPath = path.join(__dirname, "../public/manifest.json");
+const manifestJson = require(manifestPath);
+
+manifestJson.version = packageJson.version;
+
+fs.writeFileSync(manifestPath, JSON.stringify(manifestJson, null, 2));
